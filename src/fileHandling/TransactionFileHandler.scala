557d14706c49d9f869e75be83c4651972e57601e
felix.regnell@gmail.com <felix.regnell@gmail.com>
2024-03-27 23:34:30 +0100

    add debugg printfunktion

--- src/fileHandling/TransactionFileHandler.scala
+++ src/fileHandling/TransactionFileHandler.scala
@@ -4,15 +4,25 @@ class TransactionFileHandler:
   private val bankDocParser = new BankDocumentParser("textFiles/userBusinesses.txt")
   private val transactionsFileName = "textFiles/userTransactions.txt"
 
+  def printTransaction(transactions: Map[String, Vector[(String, Double)]]): Unit =
+    transactions.map((business, transactions) => 
+       println(business + ' ' + 
+       (for transaction <- transactions yield 
+        transaction._1 + ' ' + transaction._2))
+       )
+    
+
   // loads/reads bank file using a private attribute bankDockParser, uses fuction
   // storeTransaction() store.
   def loadAndStoreTransactions(fileName: String) = 
     val newTransactions: Map[String, Vector[(String, Double)]] = 
       bankDocParser.parseDocument(fileName)
 
-    val oldTransactions = ??? // Read from file specified by trasactionsFileName
+    printTransaction(newTransactions)
 
-    val combinedTransactions = ??? // new- and oldTransactions concatenated, sorting them if necessary.
+    // val oldTransactions = ??? // Read from file specified by trasactionsFileName
+
+    // val combinedTransactions = ??? // new- and oldTransactions concatenated, sorting them if necessary.
                                    // Old transactions can be assumed sorted
                                    
     // Then combinedTransactions is written to file using storeTransactions
